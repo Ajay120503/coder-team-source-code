eae830d02f46e621a91ebcb8de4c2bf11cab446d
Ajay120503 <149007089+Ajay120503@users.noreply.github.com>
2024-08-17 21:55:05 +0530

    Add files via upload

---
+++ client/src/components/Client.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Avatar from 'react-avatar';
+
+function Client({ username }) {
+
+  return (
+    <div className="d-flex align-items-center mb-3">
+      <Avatar name={username.toString()} size={40} round="50%" />
+      <span className='mx-2'>{username.toString().toUpperCase()}</span>
+    </div>
+  );
+}
+
+export default Client;
